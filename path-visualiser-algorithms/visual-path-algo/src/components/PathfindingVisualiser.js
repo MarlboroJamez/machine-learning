53d2dc42043a1356b3898ee73cae4d9bb34b4be5
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2022-01-11 21:27:10 +0200

    Node implementations
    Added Tailwindcss and setting up the state for individual nodes being rendered on mount.

---
+++ path-visualiser-algorithms/visual-path-algo/src/components/PathfindingVisualiser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Node from './PathfindingVisualiser/Node/Node'
+
+function PathfindingVisualiser() {
+    return (
+        <div className="bg-red-100">
+            Foo
+            <Node/>
+        </div>
+    )
+}
+
+export default PathfindingVisualiser
---
+++ path-visualiser-algorithms/visual-path-algo/src/components/PathfindingVisualiser/Node/Node.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Node() {
+    return (
+        <div>
+            Just a Node
+        </div>
+    )
+}
+
+export default Node
