3de6e1e0e8d4f07ed533cae9bab825af684d400c
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2022-01-13 15:00:10 +0200

    Insertion Sort Implementation

---
+++ sorting-visualiser/src/components/sortingAlgorithms/insertionSort.js
@@ -0,0 +1,23 @@
+//While advancing to the right, every bar on the left is sorted
+//for every new bar, insert it into the right place on the left
+export function getInsertionSortAnimations(array) {
+    const animations = [];
+    insertionSort(array, animations);
+    return animations;
+  }
+  
+  function insertionSort(array, animations) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i; j > 0; j--) {
+        if (array[j] < array[j - 1]) {
+          animations.push([j, j - 1]);
+          animations.push([j, j - 1]);
+          animations.push([j - 1, array[j]]);
+          animations.push([j, array[j - 1]]);
+          let temp = array[j];
+          array[j] = array[j - 1];
+          array[j - 1] = temp;
+        }
+      }
+    }
+  }
