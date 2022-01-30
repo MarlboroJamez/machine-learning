69db3fdc8ef1841eeef7fe1a7383e4d0964e07c1
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2022-01-30 14:11:45 +0200

    Modelling - Meshgrid plot

--- tensorflow-DL-or-ML/Neural-Network-Classification/02_neural_network_classification.py
+++ tensorflow-DL-or-ML/Neural-Network-Classification/02_neural_network_classification.py
@@ -43,6 +43,7 @@ Specifically, we're going to go through doing the following with TensorFlow:
 """
 
 from sklearn.datasets import make_circles
+import tensorflow as tf
 
 # Make 1000 examples
 n_samples = 2000
@@ -57,3 +58,146 @@ X
 
 # Check the labels
 y
+
+import pandas as pd
+circles = pd.DataFrame({"X0":X[:, 0], "X1":X[:, 1], "label":y})
+circles
+
+# Visualize with a plot
+import matplotlib.pyplot as plt
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu)
+
+"""### Input and output shapes"""
+
+# Check the shapes of our features and labels
+X.shape, y.shape
+
+# How many samples we're working with
+len(X), len(y)
+
+# First example of features and labels
+X[0], y[0]
+
+# Set random seed
+tf.random.set_seed(42)
+
+# 1. Create the model using the Sequential API
+model_1 = tf.keras.Sequential([
+  tf.keras.layers.Dense(1)
+])
+
+# 2. Compile the model
+model_1.compile(loss=tf.keras.losses.BinaryCrossentropy(), # binary since we are working with 2 classes (0 & 1)
+                optimizer=tf.keras.optimizers.SGD(),
+                metrics=['accuracy'])
+
+# 3. Fit the model
+model_1.fit(X, y, epochs=5)
+
+"""Looking at the accuracy metric, our model performs poorly (50% accuracy on a binary classification problem is the equivalent of guessing), but what if we trained it for longer?
+
+"""
+
+# Train our model for longer (more chances to look at the data)
+model_1.fit(X, y, epochs=200, verbose=0) # set verbose=0 to remove training updates
+model_1.evaluate(X, y)
+
+"""Even after 200 passes of the data, it's still performing as if it's guessing.
+
+What if we added an extra layer and trained for a little longer?
+"""
+
+# Set random seed
+tf.random.set_seed(42)
+
+# 1. Create the model (same as model_1 but with an extra layer)
+model_2 = tf.keras.Sequential([
+  tf.keras.layers.Dense(1), # add an extra layer
+  tf.keras.layers.Dense(1) 
+])
+
+# 2. Compile the model
+model_2.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+                optimizer=tf.keras.optimizers.SGD(),
+                metrics=['accuracy'])
+
+# 3. Fit the model
+model_2.fit(X, y, epochs=100, verbose=0) # set verbose=0 to make the output print less
+
+# Evaluate the model
+model_2.evaluate(X, y)
+
+"""### Improving our model"""
+
+# Set random seed
+tf.random.set_seed(42)
+
+# 1. Create the model (this time 3 layers)
+model_3 = tf.keras.Sequential([
+  # Before TensorFlow 2.7.0
+  # tf.keras.layers.Dense(100), # add 100 dense neurons
+
+  ## After TensorFlow 2.7.0 ##
+  tf.keras.layers.Dense(100, input_shape=(None, 1)), # add 100 dense neurons with input_shape defined (None, 1) = look at 1 sample at a time
+  tf.keras.layers.Dense(10), # add another layer with 10 neurons
+  tf.keras.layers.Dense(1)
+])
+
+# 2. Compile the model
+model_3.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+                optimizer=tf.keras.optimizers.Adam(), # use Adam instead of SGD
+                metrics=['accuracy'])
+
+# 3. Fit the model
+model_3.fit(X, y, epochs=100, verbose=0) # fit for 100 passes of the data
+
+"""We've pulled out a few tricks but our model isn't even doing better than guessing.
+
+To visualize our model's predictions we're going to create a function ```plot_decision_boundary()``` which:
+* Takes in a trained model, features (X) and labels (y).
+* Creates a [meshgrid](https://numpy.org/doc/stable/reference/generated/numpy.meshgrid.html) of the different X values.
+* Makes predictions across the meshgrid.
+* Plots the predictions as well as a line between the different zones (where each unique class falls).
+
+"""
+
+import numpy as np
+
+def plot_decision_boundary(model, X, y):
+  """
+  Plots the decision boundary created by a model predicting on X.
+  This function has been adapted from two phenomenal resources:
+   1. CS231n - https://cs231n.github.io/neural-networks-case-study/
+   2. Made with ML basics - https://github.com/GokuMohandas/MadeWithML/blob/main/notebooks/08_Neural_Networks.ipynb
+  """
+  # Define the axis boundaries of the plot and create a meshgrid
+  x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+  y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+  xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                       np.linspace(y_min, y_max, 100))
+  
+  # Create X values (we're going to predict on all of these)
+  x_in = np.c_[xx.ravel(), yy.ravel()] # stack 2D arrays together: https://numpy.org/devdocs/reference/generated/numpy.c_.html
+  
+  # Make predictions using the trained model
+  y_pred = model.predict(x_in)
+
+  # Check for multi-class
+  if len(y_pred[0]) > 1:
+    print("doing multiclass classification...")
+    # We have to reshape our predictions to get them ready for plotting
+    y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+  else:
+    print("doing binary classifcation...")
+    y_pred = np.round(y_pred).reshape(xx.shape)
+  
+  # Plot decision boundary
+  plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+  plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+  plt.xlim(xx.min(), xx.max())
+  plt.ylim(yy.min(), yy.max())
+
+"""Now we've got a function to plot our model's decision boundary (the cut off point its making between red and blue dots), let's try it out."""
+
+# Check out the predictions our model is making 
+plot_decision_boundary(model_3, X, y)
