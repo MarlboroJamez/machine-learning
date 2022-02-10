4e38f67950ac0f8412d82931d41f510e373afa0d
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2022-02-11 00:18:38 +0200

    Dashboard Implementation

--- neural-style_and_vqgd-clip-canvas/client/src/pages/dashboard.js
+++ neural-style_and_vqgd-clip-canvas/client/src/pages/dashboard.js
@@ -3,14 +3,20 @@ import React from 'react';
 //Components
 import Header from '../components/header/dashboardHeader';
 import Bot from '../components/bot/bot';
+import Actions from '../components/units/dashboardActions';
+import Gallery from '../components/units/dashboardGallery';
 
 function Dashboard() {
   return (
       <div>
           <Header/>
 
-          <div className="p-20">
+          <div className="p-6">
+            <Actions/>
 
+            <div className="mt-4 pl-6 pr-32">
+              <Gallery/>
+            </div>
           </div>
           <Bot/>
       </div>
