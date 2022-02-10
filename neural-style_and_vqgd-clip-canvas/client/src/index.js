080b3394aaa2e6ce9e0ca2bf436e1f72173367d7
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2022-02-11 00:18:18 +0200

    Added Plugins

--- neural-style_and_vqgd-clip-canvas/client/src/index.js
+++ neural-style_and_vqgd-clip-canvas/client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 
+//Tailwind
+import "@material-tailwind/react/tailwind.css";
+
 ReactDOM.render(
     <App />,
   document.getElementById('root')
